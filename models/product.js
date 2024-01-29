84427d88c48be4fd1d62c38b141589cec3793a3a
jeongyoon <developeryoon17@gmail.com>
2024-01-29 16:20:56 +0900

    nodejs project v3.0

--- models/product.js
+++ models/product.js
@@ -1,15 +1,41 @@
-const products = [];
+const fs = require('fs');
+const path = require('path'); // 모든 운영체제에서 작동하는 경로
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFormFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    products.push(this);
+    getProductsFormFile((products) => {
+      products.push(this);
+      // json파일로 변환하여 올바른형식을 지니도록 한다.
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFormFile(cb);
   }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,18 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // 시작경로를 공유
 // admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
